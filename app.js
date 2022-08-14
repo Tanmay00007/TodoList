9c1a15e728ee76c34d0e3921f497f7d688da6a81
Tanmay00007 <tanmayc2001@gmail.com>
2022-08-14 19:01:54 +0530

    commit changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDBe", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDBe", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
